4ed4409c0e2526018dbc604c2b3bc9e0472a5aaa
Rafael Aguiar <rafael.nascimento.aguiar@outlook.com>
2021-08-10 20:20:49 -0300

    added shopping-list strucure

--- src/app/recipes/components/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/components/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './../../../models/Recipe';
+import { Recipe } from '../../../../models/Recipe';
 
 @Component({
   selector: 'app-recipe-item',
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../models/Ingredient';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Batata', '500g'),
+    new Ingredient('Bacon', '100g'),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
