c1dc75050c0751d0687711ae1dd0bd14f2ada9e9
Rafael Aguiar <rafael.nascimento.aguiar@outlook.com>
2021-08-10 18:59:31 -0300

    added recipe-list and recipe-list-item components structure

--- src/app/recipes/components/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/components/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../../../models/Recipe';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.scss']
+  styleUrls: ['./recipe-item.component.scss'],
 })
 export class RecipeItemComponent implements OnInit {
+  recipe: Recipe;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/recipes/components/recipe-list/recipe-list.component.ts
+++ src/app/recipes/components/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../models/Recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Ausie Cheese Fries',
+      'Batata com Queijo e Bacon',
+      'http://images.virgula.com.br/2018/01/aussie-fries.jpg'
+    ),
+    new Recipe(
+      'Ausie Cheese Fries',
+      'Batata com Queijo e Bacon',
+      'http://images.virgula.com.br/2018/01/aussie-fries.jpg'
+    ),
+  ];
 
   constructor() {}
 
